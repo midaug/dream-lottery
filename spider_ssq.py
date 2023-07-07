742395a9486d989774f8c4921d48c489f508133f
midaug <days0814@gmail.com>
2023-07-07 11:06:43 +0800

    update

--- spider_ssq.py
+++ spider_ssq.py
@@ -2,6 +2,7 @@ import requests
 from lxml import etree
 import os
 import time
+import argparse
 
 def replaceStr(s=''):
     return s.replace(' ', '').replace(',', '')
@@ -16,72 +17,89 @@ def mkdir(path):
     else:
         return False
 
+def start(url):
+    ssqPath = './spider_data/ssq.txt'
+    ssqJSPath = './spider_data/ssq.js'
+    ssqs = {}
 
-url = 'https://datachart.500.com/ssq/history/newinc/history.php?start=00001&end=999999'
-ssqPath = './spider_data/ssq.txt'
-ssqs = {}
-
-try:
-    oldFile = open(ssqPath, 'r')
     try:
-        line = oldFile.readline()
-        while line: 
-            ssqs[line.split(',')[0]] = line
-            line = oldFile.readline() 
-    finally:
-        oldFile.close()
-except IOError:
-    print('Failed to read history, {} isNotExists \n'.format(ssqPath))
-
-
-response = requests.get(url)
-response = response.text
-selector = etree.HTML(response)
-updateSum = 0
-
-for i in selector.xpath('//tr[@class="t_tr1"]'):
-    datetime = replaceStr(i.xpath('td/text()')[0])
-    # 期数长度不符合要求时
-    if len(datetime) < 5:
-        continue
-    red = i.xpath('td/text()')[1:7]
-    blue = i.xpath('td/text()')[7]
-    prize1 = replaceStr(i.xpath('td/text()')[11])
-    prize2 = replaceStr(i.xpath('td/text()')[13])
-    drawdate = replaceStr(i.xpath('td/text()')[15])
-    #red.insert(0,'20'+datetime)
-    red.insert(0,datetime)
-    red.append(blue)
-    red.append(prize1)
-    red.append(prize2)
-    red.append(drawdate)
-    val = '{}\n'.format(','.join(red))
-    # 值异常
-    if len(val) < 26:
-        continue
-    # 已经爬取过的数据不更新
-    if datetime in ssqs and ssqs[datetime] == val:
-        continue
-    ssqs[datetime] = val
-    updateSum = updateSum + 1
-
-# 没有更新时退出
-if updateSum > 0:
-    try:
-        mkdir('./spider_data/')
-        open(ssqPath, 'w').close()
-        of = open(ssqPath,"a")
-        timeMark = '999999999'
-        ssqs[timeMark] = ''
-        ssqs[timeMark] = '{},{},{},{},{}\n'.format(timeMark, updateSum, len(ssqs.keys()) ,str(int(round(time.time() * 1000))), 'Current behavior update info')
-        ssqkeys = sorted(ssqs.keys(), reverse = True)
+        oldFile = open(ssqPath, 'r')
         try:
-            for key in ssqkeys:
-                of.write(ssqs[key])
+            line = oldFile.readline()
+            while line: 
+                ssqs[line.split(',')[0]] = line
+                line = oldFile.readline() 
         finally:
-            of.close()
-            print('updateSum is {} \n'.format(updateSum))
+            oldFile.close()
     except IOError:
-        print('Write failed')
-        
-print('Finished')
+        print('Failed to read history, {} isNotExists \n'.format(ssqPath))
+
+
+    response = requests.get(url)
+    response = response.text
+    selector = etree.HTML(response)
+    updateSum = 0
+
+    for i in selector.xpath('//tr[@class="t_tr1"]'):
+        datetime = replaceStr(i.xpath('td/text()')[0])
+        # 期数长度不符合要求时
+        if len(datetime) < 5:
+            continue
+        red = i.xpath('td/text()')[1:7]
+        blue = i.xpath('td/text()')[7]
+        prize1 = replaceStr(i.xpath('td/text()')[11])
+        prize2 = replaceStr(i.xpath('td/text()')[13])
+        drawdate = replaceStr(i.xpath('td/text()')[15])
+        #red.insert(0,'20'+datetime)
+        red.insert(0,datetime)
+        red.append(blue)
+        red.append(prize1)
+        red.append(prize2)
+        red.append(drawdate)
+        val = '{}\n'.format(','.join(red))
+        # 值异常
+        if len(val) < 26:
+            continue
+        # 已经爬取过的数据不更新
+        if datetime in ssqs and ssqs[datetime] == val:
+            continue
+        ssqs[datetime] = val
+        updateSum = updateSum + 1
+
+    # 没有更新时退出
+    if updateSum > 0:
+        try:
+            mkdir('./spider_data/')
+            open(ssqPath, 'w').close()
+            open(ssqJSPath, 'w').close()
+            of = open(ssqPath,"a")
+            ofJS = open(ssqJSPath,"a")
+            timeMark = '999999999'
+            ssqs[timeMark] = ''
+            ssqs[timeMark] = '{},{},{},{},{}\n'.format(timeMark, updateSum, len(ssqs.keys()) ,str(int(round(time.time() * 1000))), 'Current behavior update info')
+            ssqkeys = sorted(ssqs.keys(), reverse = True)
+            try:
+                ofJS.write("window.ssqData = `")
+                for key in ssqkeys:
+                    of.write(ssqs[key])
+                    ofJS.write(ssqs[key])
+                ofJS.write("`")
+            finally:
+                of.close()
+                ofJS.close()
+                print('updateSum is {} \n'.format(updateSum))
+        except IOError:
+            print('Write failed')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", type=str, required=False, default="", help="url")
+    args = parser.parse_args()
+    try:
+        print(args)
+        start(args.url)
+        print('Finished')
+    except:
+        print('error')
+
